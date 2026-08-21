f5952a8449e3f021910ce67e876aa0ccff3c59bf
AytajMammadova306 <aytajamam@code.edu.az>
2026-08-22 03:20:08 +0400

    Cinema

---
+++ CinemaReservation/CinemaReservation/CinemaHall.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaReservation
+{
+    internal class CinemaHall
+    {
+        private string movieName;
+        private int totalSeats;
+        private int reservedSeats;
+
+        public void CreateHall(string movieName, int totalSeats)
+        {
+            this.movieName = movieName;
+            this.totalSeats = totalSeats;
+        }
+        public void ReserveSeat()
+        {
+            if (RemainingSeats() > 0)
+            {
+                reservedSeats++;
+                Console.WriteLine("Resevation is succesfully made");
+            }
+            else Console.WriteLine("Sorry, we are full");
+        }
+        public void CancelReservation()
+        {
+            if (reservedSeats > 0)
+            {
+                reservedSeats--;
+                Console.WriteLine("Resevation is succesfully canceled");
+            }
+            else Console.WriteLine("There is no resevation to cancel");
+        }
+        public int RemainingSeats()
+        {
+            return totalSeats - reservedSeats;
+        }
+        public void PrintHallInfo()
+        {
+            Console.WriteLine($"Filmin adi: {movieName}, Umumi oturacaq sayi: {totalSeats}, Rezervasiya olunan oturacaq sayi: {reservedSeats}, Bos oturacaq sayi: {RemainingSeats()}");
+        }
+    }
+}
---
+++ CinemaReservation/CinemaReservation/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace CinemaReservation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*test ai ile yazilib*/
+
+
+            CinemaHall cinema = new CinemaHall();
+
+            cinema.CreateHall("Spiderman:Brand New Home", 5);
+
+            cinema.PrintHallInfo();
+
+            cinema.ReserveSeat();
+            cinema.ReserveSeat();
+
+            cinema.PrintHallInfo();
+
+            cinema.CancelReservation();
+
+            cinema.PrintHallInfo();
+
+            cinema.ReserveSeat();
+            cinema.ReserveSeat();
+            cinema.ReserveSeat();
+            cinema.ReserveSeat();
+
+            cinema.PrintHallInfo();
+
+            cinema.ReserveSeat();
+
+            cinema.CancelReservation();
+            cinema.CancelReservation();
+            cinema.CancelReservation();
+            cinema.CancelReservation();
+            cinema.CancelReservation();
+            cinema.CancelReservation();
+
+            cinema.PrintHallInfo();
+        }
+    }
+}
